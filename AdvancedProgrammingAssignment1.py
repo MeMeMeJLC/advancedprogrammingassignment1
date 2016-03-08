f6f09a8794e22b11eb100b30c98b1c5614f2cda9
Jeremy Cook <jeremy.cook.llb@live.com>
2016-03-08 20:50:49 +1300

    creating repository for assignment

---
+++ AdvancedProgrammingAssignment1.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      jeremy
+#
+# Created:     08/03/2016
+# Copyright:   (c) jeremy 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    print('hello world, this is the advanced programming assignment 1')
+
+if __name__ == '__main__':
+    main()
