66a13b5e8de64d4414f81440dadb110490fef1a9
Jeremy Cook <jeremy.cook.llb@live.com>
2016-03-08 22:17:38 +1300

    created some data handling

--- AdvancedProgrammingAssignment1.py
+++ AdvancedProgrammingAssignment1.py
@@ -8,9 +8,38 @@
 # Copyright:   (c) jeremy 2016
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
+"""
+class SalesPersonData():
+    data = [111, 'm', 22, 5000, 22, 50000, 222, 'f', 33, 1000, 5, 30000]
+    #id, gender, age, sales, bmi, income
+
+    def get_data():
+        for item in data:
+            return item
+"""
 
 def main():
     print('hello world, this is the advanced programming assignment 1')
 
+    data = [111, 'm', 22, 5000, 22, 50000, 222, 'f', 33, 1000, 5, 30000]
+
+    variables = ['id', 'gender', 'age', 'sales', 'bmi', 'income']
+
+    new_list = []
+
+    for item in data:
+        var_index = data.index(item)
+        if var_index > 5:
+            var_index = var_index % 5
+        var = variables[var_index]
+        new_list.append(var)
+        new_list.append(item)
+
+    print(new_list)
+
+
 if __name__ == '__main__':
     main()
+
+
+
