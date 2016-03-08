529450c3a6d920edb89707a1f8e757873c0afffc
Jeremy Cook <jeremy.cook.llb@live.com>
2016-03-09 00:24:14 +1300

    basic cmd added

--- AdvancedProgrammingAssignment1.py
+++ AdvancedProgrammingAssignment1.py
@@ -8,38 +8,62 @@
 # Copyright:   (c) jeremy 2016
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
-"""
-class SalesPersonData():
-    data = [111, 'm', 22, 5000, 22, 50000, 222, 'f', 33, 1000, 5, 30000]
-    #id, gender, age, sales, bmi, income
+from cmd import Cmd
 
-    def get_data():
+class Model():
+
+    def get_data_header():
+        headers = ['id', 'gender', 'age', 'sales', 'bmi', 'income']
+        return headers
+
+    def get_data_values():
+        data = [[4, 'm', 22, 5000, 22, 50000], [2, 'f', 33, 1000, 50, 30000]]
+        return data
+
+class View():
+    pass
+    def display_header(headers):
+        Model.get_data_header()
+        for head in headers:
+            print(head, end="|")
+
+
+    def display_data(data):
+        Model.get_data_values()
         for item in data:
-            return item
-"""
+            i = data.index(item)
+            print('', end='\n')
+            for item in data[i]:
+                print(item, end='   |')
+
+class Controller(Cmd):
+    pass
+    def do_greet(self, person):
+        print('hello ' + person)
+
+    def do_show_data(self, header):
+        View.display_header(Model.get_data_header())
+        View.display_data(Model.get_data_values())
+
+    def do_quit(self, args):
+        print('Quitting...')
+        raise SystemExit
+
+
+
 
 def main():
-    print('hello world, this is the advanced programming assignment 1')
-
-    data = [111, 'm', 22, 5000, 22, 50000, 222, 'f', 33, 1000, 5, 30000]
-
-    variables = ['id', 'gender', 'age', 'sales', 'bmi', 'income']
-
-    new_list = []
-
-    for item in data:
-        var_index = data.index(item)
-        if var_index > 5:
-            var_index = var_index % 5
-        var = variables[var_index]
-        new_list.append(var)
-        new_list.append(item)
-
-    print(new_list)
-
+    #print('this is the advanced programming assignment 1')
+    name = "phil"
+    #Controller.do_greet()
+    #View.display_header(Model.get_data_header())
+    #View.display_data(Model.get_data_values())
 
 if __name__ == '__main__':
     main()
+    controller = Controller()
+    controller.prompt = '> '
+    controller.cmdloop('Starting prompt...')
 
 
 
